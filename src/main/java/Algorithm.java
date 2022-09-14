a90f31084eb946e3828eb5687d8cdfe657bc186d
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-09-14 15:04:16 +0300

    add some assert

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,15 +1,43 @@
 package main.java;
 
 public class Algorithm {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Algorithm algo = new Algorithm();
         System.out.println("Expected result 2. Actual result:" + algo.timesTwo(1));
-        System.out.println("Expected result 4. Actual result:" + algo.timesTwo(2));
-        System.out.println("Expected result -4. Actual result:" + algo.timesTwo(-2));
+        assert 2 == algo.timesTwo(1);
+
+        int min = algo.findMin(new int[] {15, 4, 1});
+        assert 1 == min : "Actual result " + min;
+
+        Integer min2 = algo.findMin(new int[] {});
+        assert null == min2 : "Actual result " + min2;
+
+        Integer min3 = algo.findMin(null);
+        assert null == min3 : "Actual result " + min3;
     }
 
-    private int timesTwo(int arg){
-        return arg*2;
+    private long timesTwo(int arg) {
+        return arg * 2;
     }
+
+
+    private Integer findMin(int[] array) {
+
+        if (array !=null && array.length>0) {
+            int min = array[0];
+            for (int i = 0; i < array.length; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
 
 }
